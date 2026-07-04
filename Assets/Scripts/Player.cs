c4848e42534b794d99f52fb67467d5c1e15014bb
Эмиль <emilarullin1@gmail.com>
2026-07-04 19:08:35 +0300

    commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    
+   [SerializeField] private float movingSpeed = 10f;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 inputVector = GameInput.Instance.GetMovementVector();
+
+        inputVector = inputVector.normalized;
+
+        rb.MovePosition(rb.position + inputVector * (movingSpeed * Time.fixedDeltaTime));
+
+    }
+
+}
