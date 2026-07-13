171d6149aed945b9efc17a538a0e8e723a3cd82a
Эмиль <emilarullin1@gmail.com>
2026-07-13 21:20:52 +0300

    isRunningAnimationPart1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
-   [SerializeField] private float movingSpeed = 10f;
+
+    [SerializeField] private float movingSpeed = 10f;
 
     private Rigidbody2D rb;
+
+    private float minMovingSpeed = 0.1f;
+
+    private bool isRunnig = false;
 
     private void Awake()
     {
@@ -16,12 +20,23 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
         Vector2 inputVector = GameInput.Instance.GetMovementVector();
-
         inputVector = inputVector.normalized;
-
         rb.MovePosition(rb.position + inputVector * (movingSpeed * Time.fixedDeltaTime));
 
+        if (Mathf.Abs(inputVector.x) > minMovingSpeed || Mathf.Abs(inputVector.y) > minMovingSpeed)
+        {
+            isRunnig = true;
+        }
+        else
+        {
+            isRunnig = false;
+        }
     }
 
 }
---
+++ Assets/Scripts/PlayerVisual.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerVisual : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        
+    }
+}
